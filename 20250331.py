1ea859042592c88a7618a5a1e26cdf59b85cdf12
wonpark1 <2023111927@dgu.ac.kr>
2025-03-31 18:18:09 +0900

    oss2 브랜치 작업 완료

--- 20250331.py
+++ 20250331.py
@@ -1,9 +1,8 @@
-name = input("이름을 입력하세요: ")
-student_id = input("학번을 입력하세요: ")
-subject=input("학과를 입력하세요:")
+name = input("이름: ")
+student_id = input("학번: ")
 # 결과 출력
 print()
 print("<출력>")
 print("이름:", name)
 print("학번:", student_id)
-print("학과:",subject)
+
